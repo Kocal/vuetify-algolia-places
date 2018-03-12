0b5e87872fba889ad0dd379722add9d2c5f99a71
Hugo Alliaume <kocal@live.fr>
2018-03-12 22:10:43 +0100

    doc: add simple example

---
+++ docs/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import App from './App.vue';
+
+Vue.use(Vuetify);
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+});
